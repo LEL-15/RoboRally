9b7c7d93128a3c3e5c7587d4d00ae10377fe3263
gerrittluoma <43622528+gerrittluoma@users.noreply.github.com>
2020-02-26 15:01:07 -0700

    almost finished init()

--- Lab_4/lab4_base.py
+++ Lab_4/lab4_base.py
@@ -27,14 +27,13 @@ CYCLE_TIME = 0.1 # In seconds
 def main():
     global publisher_motor, publisher_ping, publisher_servo, publisher_odom
     global IR_THRESHOLD, CYCLE_TIME
-    CYCLE_TIME = 50;
     global pose2d_sparki_odometry
+    global theta, value_array, ping_distance
 
     #TODO: Init your node to register it with the ROS core
     init()
 
     while not rospy.is_shutdown():
-        start_time = int(round(time.time() * 1000))
         #TODO: Implement CYCLE TIME
 
         #TODO: Implement line following code here
@@ -46,34 +45,45 @@ def main():
             rospy.loginfo("Loop Closure Triggered")
 
         #TODO: Implement CYCLE TIME
-        end_time = int(round(time.time() * 1000));
-        if(end_time - start_time < 50):
-            rospy.sleep((50 - (start_time - end_time)) / 1000);
+        rospy.sleep(0)
+
 
 
 def init():
     global publisher_motor, publisher_ping, publisher_servo, publisher_odom
     global subscriber_odometry, subscriber_state
     global pose2d_sparki_odometry
+    global theta, value_array, ping_distance
     publisher_motor = rospy.Publisher('/sparki/motor_command', 'std_msgs/Float32MultiArray')
     publisher_ping = rospy.Publisher('/sparki/ping_command', 'std_msgs/Empty')
     publisher_servo = rospy.Publisher('/sparki/set_servo', 'std_msgs/Int16')
     publisher_odom = rospy.Publisher('/sparki/set_odometry', 'geometry_msgs/Pose2D')
-    subscriber_odometry = rospy.Subscriber('/sparki/odometry', 'geometry_msgs/Pose2D')
-    subscriber_state = rospy.Subscriber('/sparki/state', 'std_msgs/String')
+    subscriber_odometry = rospy.Subscriber('/sparki/odometry', 'geometry_msgs/Pose2D', callback_update_odometry)
+    subscriber_state = rospy.Subscriber('/sparki/state', 'std_msgs/String', callback_update_state)
 
+    msg = Int16()
+    msg.data = 45
+    publisher_servo.publish(msg)
     #TODO: Set up your publishers and subscribers
     #TODO: Set up your initial odometry pose (pose2d_sparki_odometry) as a new Pose2D message object
     #TODO: Set sparki's servo to an angle pointing inward to the map (e.g., 45)
 
 def callback_update_odometry(data):
     # Receives geometry_msgs/Pose2D message
-    global pose2d_sparki_odometry
+    global pose2d_sparki_odometry 
+    pose2d_sparki_odometry = data.data
     #TODO: Copy this data into your local odometry variable
 
 def callback_update_state(data):
+    global theta, value_array, ping_distance
     state_dict = json.loads(data.data) # Creates a dictionary object from the JSON string received from the state topic
     #TODO: Load data into your program's local state variables
+    theta = state_dict["servo"]
+    value_array = state_dict["light_sensors"]
+    try:
+        ping_distance = state_dict["ping_distance"]
+    except:
+        ping_distance = None
 
 def convert_ultrasonic_to_robot_coords(x_us):
     #TODO: Using US sensor reading and servo angle, return value in robot-centric coordinates
